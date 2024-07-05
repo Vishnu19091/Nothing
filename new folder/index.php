ed878e0c3cac90b8912641a83eb505acafc02322
Vishnu19091 <vishnusah368@gmail.com>
2024-07-05 19:00:34 +0530

    Added a simple login form in new folder

---
+++ new folder/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles.css" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    />
+    <title>Simple dev</title>
+    <script src="script.js" type="text/javascript"></script>
+  </head>
+
+  <body class="grad1">
+    <div class="container">
+      <form name="login" action="display.php" method="get" onsubmit="return validate()">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" />
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" />
+        <label for="phnum"><b>Phone Number</b></label>
+        <input type="number" placeholder="Enter Phone Number" name="phnum" />
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" />
+        <!-- <button type="submit" class="btn" value="submit">Login</button> -->
+        <input type="submit"/>
+        <a href="#link">Forget Password</a>
+      </form>
+    </div>
+    <div class="modal hidden">
+      <div class="overlay hidden" id="displayArea"></div>
+    </div>
+  </body>
+</html>
---
+++ new folder/sample.php
@@ -0,0 +1,28 @@
+<?php
+$servername ="127.0.0.1";
+$username ="root";
+$password ="";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection failed" . $conn->connect_error);
+}
+else
+echo"connected Successfully";
+
+$sql = "SELECT id, firstname, lastname FROM Sample_Table";
+$result=$conn->query($sql);
+
+if($result->num_rows > 0){
+    while($row=$result->fetch_assoc()){
+    echo "<br><br>"."id: " . $row["id"] ."<br>"."FirstName: " . $row["firstname"] .
+    "<br>"."LastName: " . $row["lastname"] . "<br><br>";
+} }
+else {
+    echo"no results found";
+}
+
+$conn->close();
+?>
